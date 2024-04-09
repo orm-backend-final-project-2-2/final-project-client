d72f2ae51bbf30aa412e5045d16c713bdd7b81f1
ZeyaKim <chlwltjrdev@gmail.com>
2024-04-10 00:44:42 +0900

    Add WebMainScreen widget

---
+++ lib/views/screens/web_main_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:codefit/views/widgets/web/web_header.dart';
+import 'package:codefit/views/widgets/web/web_footer.dart';
+import 'package:codefit/views/widgets/common/main_content.dart';
+
+class WebMainScreen extends StatelessWidget {
+  const WebMainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      child: Column(
+        children: [
+          WebHeader(),
+          MainContent(),
+          WebFooter(),
+        ],
+      ),
+    );
+  }
+}
