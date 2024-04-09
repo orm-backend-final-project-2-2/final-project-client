401750f645be2847d994bb597072cf167b191611
ZeyaKim <chlwltjrdev@gmail.com>
2024-04-10 00:42:37 +0900

    ✨ Add MainScreen widget to handle platform-specific UI in main_screen.dart

---
+++ lib/views/screens/main_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:codefit/views/screens/android_main_screen.dart';
+import 'package:codefit/views/screens/web_main_screen.dart';
+
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: kIsWeb ? WebMainScreen() : AndroidMainScreen(),
+    );
+  }
+}
