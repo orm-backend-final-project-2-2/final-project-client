b37b8462cf860f3d56a9bdf625d12b28bf2502e0
ZeyaKim <chlwltjrdev@gmail.com>
2024-04-10 23:30:55 +0900

    Fix formatting in MainScreen widget

--- lib/views/screens/main_screen.dart
+++ lib/views/screens/main_screen.dart
@@ -3,14 +3,13 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:codefit/views/screens/android_main_screen.dart';
 import 'package:codefit/views/screens/web_main_screen.dart';
 
-
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: kIsWeb ? WebMainScreen() : AndroidMainScreen(),
+      body: kIsWeb ? WebMainScreen() : AndroidMainScreen()
     );
   }
 }
