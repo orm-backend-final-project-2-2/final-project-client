f1472099050addfb6f9e5830b887f9794acd2da4
ZeyaKim <chlwltjrdev@gmail.com>
2024-04-10 23:32:20 +0900

    Add CommunityContent widget

---
+++ lib/views/widgets/common/contents/community_content.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CommunityContent extends StatelessWidget {
+  const CommunityContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            height: 200,
+          )
+        ],
+      ),
+    );
+  }
+}
