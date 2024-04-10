4e6d01d0534899e02df3ca2f79a5fb3ca36fa330
ZeyaKim <chlwltjrdev@gmail.com>
2024-04-10 23:32:04 +0900

    Refactor CustomNavItemList widget to use customNavItems in custom_nav_item.dart

--- lib/views/widgets/common/custom_nav_item.dart
+++ lib/views/widgets/common/custom_nav_item.dart
@@ -7,17 +7,12 @@ class CustomNavItemInfo {
   CustomNavItemInfo({
     required this.label,
     required this.icon,
-    
   });
 }
 
-class CustomNavItemList { // 필요한 경우 T를 여기서도 사용할 수 있습니다.
-  List<CustomNavItemInfo> items = [
-    CustomNavItemInfo(label: 'Home', icon: Icons.home),
-    CustomNavItemInfo(label: 'Profile', icon: Icons.person),
-    CustomNavItemInfo(label: 'Settings', icon: Icons.settings),
-  ];
-  VoidCallback? onTap;
-
-  CustomNavItemList();
-}
+List<CustomNavItemInfo> customNavItems = [
+  CustomNavItemInfo(label: 'Home', icon: Icons.home),
+  CustomNavItemInfo(label: 'Search', icon: Icons.search),
+  CustomNavItemInfo(label: 'Favorite', icon: Icons.favorite),
+  CustomNavItemInfo(label: 'Settings', icon: Icons.settings),
+];
